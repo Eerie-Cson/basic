4103d053b3dce1186e103a7678266b67172f2fb6
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-03-18 12:37:08 +0800

    Revert "refactor: apply reduce on basic array, throw error on empty array (#2)"
    This reverts commit 0c3ce412517d549d5a45ce2e42f0aab4bf92d306.

--- basic/basic-array.js
+++ basic/basic-array.js
@@ -1,12 +1,16 @@
 class BasicArray{
-  sum(numbers){
-    if(numbers.length==0){
-      throw new Error("Empty Array!");
-    }
-    return numbers.reduce((accum, currentValue) => accum+currentValue, 0);
-
-  }
-}
-
-
-module.exports = BasicArray;
+    sum(number=[]){
+        let total = 0, counter = number.length-1;        //initialize total with 0 and counter with the 
+                                                         //total number of elements of an array reduced by 1
+        while(counter>=0){
+             total = total + number[counter];                 //Calculate the total of the elements by looping through the array 
+             counter--;
+         }
+         return total;                                   //display the total value accumulated from the total variable
+                                                      //after the loop
+     }
+ }
+ 
+ 
+ module.exports = BasicArray;
+ 
