dd89bb1fa7e5d5d3b49c5d326f930dad43a0e910
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-03-18 13:14:11 +0800

    refactor: apply map and join on basicString#repeat

--- basic/basic-string.js
+++ basic/basic-string.js
@@ -1,13 +1,6 @@
 class BasicString{
-    repeat(string){
-
-        let stringHolder = "", stringCount = 0;        //initialize string holder and string counter
-        while(stringCount<5){                           
-            stringHolder = stringHolder + string;       //fill the string holder with string 'a' || 'ab'
-            stringCount++;                              //increment the string counter until 5
-        }
-        return stringHolder;                            //display all the string accumulated by stringholder after 
-                                                        //the loop
+    repeat(str){
+      return [...Array(5)].map(e => e = str).join("");
     }
 }
 
