c70e08e1c427d67d53d600515340cc696acb3da9
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-03-24 09:36:36 +0800

    refactor: refactor using ramda library

--- basic/objects/simple-wallet.js
+++ basic/objects/simple-wallet.js
@@ -1,19 +1,17 @@
+const R = require('ramda');
 class Wallet{
   balance = 0;
 
   deposit(newBalance){
     if(newBalance<=0)
       throw new Error("No value to deposit!");
-
-    this.balance += ~~newBalance;
-    return this.balance;
+    return this.balance = R.add(this.balance, ~~newBalance);
   }
   
   withdraw(newBalance){
     if(newBalance>this.balance)
       throw new Error("Insufficient Balance!");
-    this.balance -= ~~newBalance;
-    return this.balance;
+    return this.balance = R.subtract (this.balance, ~~newBalance);
   }  
 };
 module.exports =  Wallet;
