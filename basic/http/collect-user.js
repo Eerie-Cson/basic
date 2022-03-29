afff73cd0e91e9d1e5d9a82b1313294f6edc6f87
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-03-29 11:06:43 +0800

    test: accept the password string and convert to argon2 hash

--- basic/http/collect-user.js
+++ basic/http/collect-user.js
@@ -1,27 +1,44 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const {v4 : uuidv4} = require('uuid');
+const argon2 = require('argon2');
 
 const app = new Koa(),
  router =  new Router();
+
+
+async function hashPassword(password){
+  try {
+    const hash = await argon2.hash(password);
+    return hash;
+  } catch (err) {
+    throw new Error('Error');
+  }
+}
 
 class User {
   data =  {    
     "users":
       [{
-        "id":"1",
+        "id": uuidv4(),
         "name":"the name",
-        "email":"thename@gmail.com",
-        "password":"random string"
+        "email":"thename@gmail.com"
       }]
   };
 
-};
+  getUsersPass(){
 
-const user = new User(); 
+    return this.data.users[0].password;
+  }
+
+
+};
+const user = new User();
+
 router.get( '/users', async ctx => {
-  ctx.body = user;
+  ctx.body = user.getUsersPass();
 });
 app.use(router.routes());
 
-module.exports = app;
+module.exports = {app, hashPassword};
 
--- basic/http/collect-user.test.js
+++ basic/http/collect-user.test.js
@@ -1,12 +1,22 @@
 const request = require('supertest');
 const app = require('./collect-user');
+const hashPassword = require('./collect-user');
+
 
 describe('app', () => {
+  afterAll(() => {
+    server.close();
+  });
+
   const server = app.listen(8080);
   
   it('should have the response status of 200', async () => {
     const response = await request(server).get('/users');
     expect(response.status).toBe(200)
+  });
+
+  it('should accept string password', async () => {
+    expect(hashPassword("random string")).toBe(String);
   });
 
   it('should have the correct response body', async () => {
@@ -15,14 +25,14 @@ describe('app', () => {
       "data": {
         "users": [
           {
-            "id": "1",
+            "id": 1,
             "name": "the name",
             "email": "thename@gmail.com",
             "password": "random string"
           }
         ]
       }
-    });
+    })
   });
   
 });
