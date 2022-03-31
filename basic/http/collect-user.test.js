deea77da12e7bfad9f2198c0cfd1e96791d81ed2
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-03-31 11:22:01 +0800

    feat: get user by ID (#22)
    * feat: get user by ID
    
    * fix: add get user by ID implementation

--- basic/http/collect-user.test.js
+++ basic/http/collect-user.test.js
@@ -1,8 +1,7 @@
 const request = require('supertest');
-const argon2 = require('argon2');
 const { app, hashPassword} = require('./collect-user');
+const argon2 = require('argon2');
 const {Chance} = require('chance');
-const { expect } = require('@jest/globals');
 
 const chance = new Chance();
 
@@ -10,11 +9,9 @@ describe('app', () => {
   afterAll(() => {
     server.close();
   });
-  
 
   const server = app.listen(8080);
 
-  
   it('should have the response status of 200', async () => {
     const response = await request(server).get('/users');
     expect(response.status).toBe(200)
@@ -25,9 +22,9 @@ describe('app', () => {
     const verified = await argon2.verify(hashedPassword, str);
     expect(verified).toBe(true);
   });
+
   it('should not display password', async () => {
     const response = await request(server).get('/users');
-    console.log(response.body.data);
     expect(Object.keys(response.body.data[0])).toStrictEqual(["id","name"]);    
   });
 
@@ -46,9 +43,14 @@ describe('app', () => {
       const firstUser = fetchResponse.body.data[0];
       const updateResponse = await request(server).patch(`/users/${firstUser.id}`).send({name: 'hello'});
       expect(updateResponse.body.data[0].name).toBe('hello');
-      expect(updateResponse.body.data[0].name).not.toStrictEqual(firstUser.name);
-    
-  })
+      expect(updateResponse.body.data[0].name).not.toStrictEqual(firstUser.name);    
+  });
   
-  
+  it('should display the corresponding data of the id', async () => {
+    const fetchResponse = await request(server).get('/users');
+    const secondUser = fetchResponse.body.data[1];
+    const response = await request(server).get(`/users/${secondUser.id}`);
+    expect(response.body.data).toStrictEqual(secondUser);
+  });
+
 });
