88f8b9f3aa631d4794ecac41a00620513737aba4
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-03-31 09:40:13 +0800

    feat: add another object with a password (#20)
    * feat: add another object with a password
    
    * refactor: add post endpoint
    
    * feat: add update endpoit
    
    * refactor: update user given the placeholder of :id
    
    * refactor: update first User name using id
    
    * refactor: refactor update test case, update array of object
    
    * fix(update endpoint): fix update test case and implementation

--- basic/http/collect-user.test.js
+++ basic/http/collect-user.test.js
@@ -1,6 +1,10 @@
 const request = require('supertest');
 const argon2 = require('argon2');
 const { app, hashPassword} = require('./collect-user');
+const {Chance} = require('chance');
+const { expect } = require('@jest/globals');
+
+const chance = new Chance();
 
 describe('app', () => {
   afterAll(() => {
@@ -23,7 +27,28 @@ describe('app', () => {
   });
   it('should not display password', async () => {
     const response = await request(server).get('/users');
+    console.log(response.body.data);
     expect(Object.keys(response.body.data[0])).toStrictEqual(["id","name"]);    
   });
+
+  it('should add another object with a password', async () => {
+    const name = chance.name(),
+    id = chance.natural();
+  
+    const response = await request(server).post('/users').send({name, id});
+    expect(response.body.data[response.body.data.length-1])
+      .toStrictEqual({name, id});
+  });
+
+  it('should update existing object', async() =>{  
+
+      const fetchResponse = await request(server).get(`/users`);
+      const firstUser = fetchResponse.body.data[0];
+      const updateResponse = await request(server).patch(`/users/${firstUser.id}`).send({name: 'hello'});
+      expect(updateResponse.body.data[0].name).toBe('hello');
+      expect(updateResponse.body.data[0].name).not.toStrictEqual(firstUser.name);
+    
+  })
+  
   
 });
