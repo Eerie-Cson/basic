b21133787a56afbd0fb5f923ec96f26d1b15ff0b
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-03-31 10:24:51 +0800

    feat: get user by ID

--- basic/http/collect-user.js
+++ basic/http/collect-user.js
@@ -6,12 +6,12 @@ const R = require('ramda');
 const argon2 = require('argon2');
 
 const app = new Koa(),
- router =  new Router();
-const chance = new Chance();
+      router =  new Router(),
+      chance = new Chance();
 
 app.use(BodyParser());
 
- async function hashPassword(str){
+async function hashPassword(str){
   const hash = await argon2.hash(str);
   return hash;
 }
@@ -27,52 +27,45 @@ class User {
 
 };
 
-const user = new User();
-const userData = user.data;
+const user = new User(),
+      userData = user.data;
 
 router.get( '/users', async ctx => {
   ctx.body = {
     data:userData.map((e)=>{
       return {id:e.id,name:e.name}; 
     })
-  };
+  }
 });
 
 router.post('/users',async ctx => {
-  
   userData.push(ctx.request.body);
-  
   ctx.body = {
     data:userData.map((e)=>{
       return {id:e.id,name:e.name}; 
     })
-  };
+  }
 });
 
 router.patch('/users/:id',async ctx => {  
-
   for (const obj of userData) {
 
     if (obj.id == ctx.params.id) {
       obj.name = ctx.request.body.name;
       break;
-    }
-    
-  }
+    }  
+  };
  
   const updated = userData;
   ctx.body = {
     data:updated.map((e)=>{
       return {id:e.id,name:e.name}; 
     })
-  }
-  
+  }  
 });
 
 app.use(router.routes());
-
 module.exports = {
   app,
   hashPassword
 };
-
