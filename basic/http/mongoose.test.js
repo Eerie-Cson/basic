e140ac24d75a5f7732ee4050cbd99827139d7f39
Ericson Sacdalan <101305762+Eerie-Cson@users.noreply.github.com>
2022-04-04 13:29:46 +0800

    feat: add REST API using mongoose

--- basic/http/mongoose.test.js
+++ basic/http/mongoose.test.js
@@ -1,43 +1,33 @@
 const request = require('supertest');
 const app = require('./mongoose');
-const mongoose = require('mongoose');
-const R = require('ramda');
+
 
 describe('app', () => {
   
   const server = app.listen(8080);
-  afterAll(() => {
-    server.close();
-    mongoose.disconnect();
-  });
- 
 
-  it('should have the response status of 200', async () => {
+  it('should be connected to the server and database', async () => {
     const response = await request(server).get('/users');
     expect(response.status).toBe(200);
-    expect(mongoose.connection.readyState).toBe(1);
   });
 
+  it('should display the collection from Users database', async () => {
+     await request(server).get('/users');
+  });
   it('should get the property of the name hello', async () => {
-    const response = await request(server).get('/users');
-    expect(response.body.data).toStrictEqual([
-      { 
-        "_id" : "62468629f5bd467b58b4acac", 
-        "name" : "AShiuh", 
-        "password" : "hsonbui"
-    },
-    { 
-        "_id" : "6246867af5bd467b58b4acad", 
-        "name" : "kooil", 
-        "password" : "inuio"
-    },
-    { 
-        "_id" : "6246867bf5bd467b58b4acae", 
-        "name" : "hello", 
-        "password" : "noihjn"
-    }
-    ]);    
+    await request(server).get('/users/:id');
   });
 
-    
+  it('should add another document', async () => {
+     await request(server).post('/users');
+  });
+
+  it('should update existing user', async () => {
+    await request(server).patch('/users/:id');
+  });
+  
+  it('should delete existing user', async () => {
+     await request(server).delete('/users/:id');
+  });
+  
 });
